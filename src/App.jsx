a063667c5b9fed1b03684306c42a9551d06224ea
lucas20salles <lucassalles199@hotmail.com>
2026-01-01 21:49:12 -0300

    react com firebase

---
+++ src/App.jsx
@@ -0,0 +1,162 @@
+import { db } from "./firebaseConnection";
+import { useState } from "react";
+import {
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore"; // Adicionar dados ao Cloud Firestore
+
+import "./app.css";
+
+function App() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [idPosts, setIdPosts] = useState("");
+
+  const [posts, setPosts] = useState([]);
+
+  async function handleAdd() {
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    //   .then(() => {
+    //     alert("DADOS REGISTRADO NO BANCO!");
+    //   })
+    //   .catch((error) => {
+    //     alert("GEROU ERRO" + error);
+    //   });
+
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        alert("DADOS REGISTRADO NO BANCO!!!");
+        setAutor("");
+        setTitulo("");
+      })
+      .catch((error) => {
+        alert("GEROU ERRO" + error);
+      });
+  }
+
+  async function buscarPosts() {
+    // const postRef = doc(db, "posts", "12345");
+    // await getDoc(postRef)
+    //   .then((snapshot) => {
+    //     setAutor(snapshot.data().autor);
+    //     setTitulo(snapshot.data().titulo);
+    //   })
+    //   .catch(() => {
+    //     alert("ERRO AO BUSCAR");
+    //   });
+
+    const postRef = collection(db, "posts");
+    await getDocs(postRef)
+      .then((snapshot) => {
+        let lista = [];
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+        });
+        setPosts(lista);
+      })
+      .catch((error) => {
+        alert("DEU ALGUM ERRO AO BUSCAR OS POSTS" + error);
+      });
+  }
+
+  async function atualizarPost() {
+    const idRef = doc(db, "posts", idPosts);
+    await updateDoc(idRef, {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        alert("POST ATUALIZADO COM SUCESSO!!");
+        setIdPosts("");
+        setTitulo("");
+        setAutor("");
+      })
+      .catch((error) => {
+        alert(`ERRO AO ATUALIZAR O POST"\n\n${error}`);
+      });
+  }
+
+  async function deletarPosts(id) {
+    const docRef = doc(db, "posts", id);
+    await deleteDoc(docRef)
+      .then(() => {
+        alert("DELETOU ARQUIVO COM SUCESSO");
+        buscarPosts();
+      })
+      .catch((error) => {
+        alert(`DEU ERRO AO DELETAR\n\n${error}`);
+      });
+  }
+
+  return (
+    <div>
+      <h1>ReactJs + Firebase :)</h1>
+
+      <div className="container">
+        <label>ID do Post:</label>
+        <input
+          type="text"
+          placeholder="Digite o ID que deseja atualizar!!"
+          value={idPosts}
+          onChange={(event) => setIdPosts(event.target.value)}
+        />
+
+        <label>Titulo:</label>
+        <textarea
+          type="text"
+          placeholder="Digite o titulo"
+          value={titulo}
+          onChange={(event) => setTitulo(event.target.value)}
+        ></textarea>
+        <label>Autor:</label>
+        <input
+          type="text"
+          placeholder="Autor do post"
+          value={autor}
+          onChange={(event) => setAutor(event.target.value)}
+        />
+        <div className="btn">
+          <button onClick={handleAdd}>Cadastrar</button>
+          <button onClick={buscarPosts}>Buscar posts</button>
+          <button onClick={atualizarPost}>Atualizar o Post</button>
+        </div>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <strong>ID: {post.id}</strong>
+                <span>Titulo: {post.titulo}</span>
+                <span>Autor: {post.autor}</span>
+                <button
+                  className="deletarBtn"
+                  onClick={() => deletarPosts(post.id)}
+                >
+                  Deletar
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseConnection.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"; // função para iniciar o firebase em uma aplicação
+import { getFirestore } from "firebase/firestore"; // leitura de dados em um banco de dados do cloud firestore
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxKUjd2f7l4cp6or89Liu_HOWZ3NZkCIw",
+  authDomain: "curso-b6c76.firebaseapp.com",
+  projectId: "curso-b6c76",
+  storageBucket: "curso-b6c76.firebasestorage.app",
+  messagingSenderId: "90242871312",
+  appId: "1:90242871312:web:0b5249039efc09c3a31a95",
+  measurementId: "G-40N524VMH9",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { db };
